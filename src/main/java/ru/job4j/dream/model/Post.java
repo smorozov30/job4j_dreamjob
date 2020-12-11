28caf97b8c91b502b1c5fb09a653f31983007e7d
Sergey Morozov <morozov.java.job@gmail.com>
2020-12-11 09:33:33 +0400

    Scriplet. Динамическая таблица. [#428521]

---
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -0,0 +1,67 @@
+package ru.job4j.dream.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate created;
+
+    public Post(int id, String name, String description, LocalDate created) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDate created) {
+        this.created = created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -0,0 +1,28 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Post;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Store {
+    private static final Store INST = new Store();
+
+    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private Store() {
+        posts.put(1, new Post(1, "Junior Java Job", "Вакансия Junior Java", LocalDate.now()));
+        posts.put(2, new Post(2, "Middle Java Job", "Вакансия Middle Java", LocalDate.now()));
+        posts.put(3, new Post(3, "Senior Java Job", "Вакансия Senior Java", LocalDate.now()));
+    }
+
+    public static Store instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
