d413c90510f4e47214eed7e6aa66a51adfba6a6e
Sergey Morozov <morozov.java.job@gmail.com>
2020-12-13 11:37:25 +0400

    CandidateSerlvet. Создание кандидата. [#428533]

--- src/main/java/ru/job4j/dream/model/Candidate.java
+++ src/main/java/ru/job4j/dream/model/Candidate.java
@@ -6,6 +6,11 @@ public class Candidate {
     private int id;
     private String name;
 
+    public Candidate(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
