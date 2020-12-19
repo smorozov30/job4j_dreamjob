8b91b7610cb03dbfc1575753949ed6eab7b9e714
Sergey Morozov <morozov.java.job@gmail.com>
2020-12-19 15:57:42 +0400

    CORS [#428509]

---
+++ src/main/java/ru/job4j/dream/filter/CORSFilter.java
@@ -0,0 +1,35 @@
+package ru.job4j.dream.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CORSFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
+
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        if (httpServletRequest.getMethod().equals("OPTIONS")) {
+            resp.setStatus(HttpServletResponse.SC_ACCEPTED);
+            return;
+        }
+
+        chain.doFilter(httpServletRequest, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
